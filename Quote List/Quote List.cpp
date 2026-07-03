5010006bfeebde4c5a95b3be831603bf04409596
BlackDragon3129 <dayte.akkaunt.2010@gmail.com>
2026-07-03 20:11:18 +0300

    Wrote quotes tests

--- Quote List/Quote List.cpp
+++ Quote List/Quote List.cpp
@@ -1,12 +1,32 @@
-﻿// Quote List.cpp: определяет точку входа для приложения.
-//
+﻿#include <iostream>
+#include <exception>
+#include <clocale>
 
-#include <iostream>
+#include <Quote.hpp>
 
 using namespace std;
+using namespace QuoteList;
 
 int main()
 {
-	cout << "Hello CMake." << endl;
+	std::setlocale(LC_ALL, "");
+
+	try
+	{
+		Quote wrongQuote("");
+	}
+	catch (std::invalid_argument)
+	{
+		std::cout << "Caught invalid_argument in the wrong quote" << std::endl;
+	}
+
+	Quote unknownQuote("Just a test quote");
+	Quote myQuote("Just a test quote №2", "Me");
+	Quote cppQuote("Just a test quote №3", "", "Quote list");
+
+	unknownQuote.Print();
+	myQuote.Print();
+	cppQuote.Print();
+
 	return 0;
 }

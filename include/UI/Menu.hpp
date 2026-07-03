93160543c9aba07871bbe6230e5d56ef93aa023c
BlackDragon3129 <dayte.akkaunt.2010@gmail.com>
2026-07-03 21:13:14 +0300

    Wrote Menu's functions

--- include/UI/Menu.hpp
+++ include/UI/Menu.hpp
@@ -1,10 +1,27 @@
 #pragma once
+#include <vector>
+
+#include <UI/MenuOption.hpp>
+#include <BaseLibrary.hpp>
 
 
 namespace QuoteList
 {
-	class Menu
+	namespace UI
 	{
+		class Menu
+		{
+			std::vector<MenuOption> m_options;
+			const char* m_name;
+			unsigned int m_currentOption;
 
-	};
+		public:
+			Menu(const char* name);
+
+			void AddOption(const MenuOption& newOption);
+			void ChangeCurrentOption(const int& change);
+			void Draw() const;
+			void OptionAct();
+		};
+	}
 }

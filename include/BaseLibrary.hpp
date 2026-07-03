d05f7388323d59201a07f3a7df51c604af23ca4d
BlackDragon3129 <dayte.akkaunt.2010@gmail.com>
2026-07-03 21:05:07 +0300

    Created Math Module

--- include/BaseLibrary.hpp
+++ include/BaseLibrary.hpp
@@ -5,6 +5,34 @@
 
 namespace QuoteList
 {
+	struct Math
+	{
+		template<typename T>
+		static T Clamp(const T& val, const T& min, const T& max)
+		{
+			static_assert(std::is_arithmetic_v<T>, "Math::Clamp T must be a number type");
+
+			if (val < min)
+				return min;
+			else if (val > max)
+				return max;
+			return val;
+		}
+
+		template<typename T>
+		static T ClampReverse(const T& val, const T& min, const T& max)
+		{
+			static_assert(std::is_arithmetic_v<T>, "Math::Clamp T must be a number type");
+
+			if (val < min)
+				return max;
+			else if (val > max)
+				return min;
+			return val;
+		}
+	};
+
+
 	static void CursorGotoXY(const unsigned int& x, const unsigned int& y)
 	{
 		COORD coord = COORD();

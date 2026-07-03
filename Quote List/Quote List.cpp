28220913330c78f791b87663b3e2574c5c7aff23
BlackDragon3129 <dayte.akkaunt.2010@gmail.com>
2026-07-03 22:42:48 +0300

    Wrote quotes creation

--- Quote List/Quote List.cpp
+++ Quote List/Quote List.cpp
@@ -14,6 +14,14 @@ using namespace std;
 using namespace QuoteList;
 
 
+
+// The list of quotes
+std::vector<Quote> quotes;
+// Menu
+UI::Menu menu("Quotes List");
+
+
+
 static std::string Input(const char* prompt)
 {
 	std::string input;
@@ -25,24 +33,43 @@ static std::string Input(const char* prompt)
 }
 
 
+static void CreateQuote(std::vector<Quote>* quotesList, UI::Menu* menu)
+{
+	system("cls");
+
+	std::string content, author, source;
+
+	content = Input("Enter quote's content: ");
+	if (content == "")
+	{
+		std::cout << "Quote must be not empty!" << std::endl;
+		Sleep(2.0f);
+		return;
+	}
+
+	author = Input("Enter quote's author (if the author is unknown, leave the field empty): ");
+	source = Input("Enter quote's source (if the source is unknown, leave the field empty): ");
+
+	quotesList->push_back(Quote(content, author, source));
+
+	std::cout << "The quote has been successfully created!" << std::endl;
+	Sleep(2.0f);
+
+	menu->Draw();
+}
+
+
 int main()
 {
 	std::setlocale(LC_ALL, "");
 
-	// The list of every quote
-	std::vector<Quote> quotes;
-
 	// Menu Test
-	UI::Menu menu("Quotes List");
-
-	menu.AddOption(UI::MenuOption("Add quote", []() {} ));
+	menu.AddOption(UI::MenuOption("Add quote", []() { CreateQuote(&quotes, &menu); }));
 	menu.AddOption(UI::MenuOption("Quotes list", []() {}));
 	menu.AddOption(UI::MenuOption("Import quotes", []() {}));
 	menu.AddOption(UI::MenuOption("Settings", []() {}));
 	menu.AddOption(UI::MenuOption("GitHub", []() {}));
 	menu.AddOption(UI::MenuOption("Exit", []() { exit(0); }));
-
-	menu.Draw();
 
 	menu.Open();
 

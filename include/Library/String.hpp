22c40a93f513e11a2c2e902fc7ff9711f7c1de91
BlackDragon3129 <dayte.akkaunt.2010@gmail.com>
2026-07-08 20:45:29 +0300

    Moved string functions to the special file

--- include/Library/String.hpp
+++ include/Library/String.hpp
@@ -1,1 +1,25 @@
 #pragma once
+
+#include <iostream>
+
+
+namespace QuoteList
+{
+	struct String
+	{
+		static std::vector<std::wstring> Split(const std::wstring& str, const wchar_t& delimeter)
+		{
+			std::wstringstream stream(str);
+			std::wstring item;
+
+			std::vector<std::wstring> elements;
+
+			while (std::getline(stream, item, delimeter))
+			{
+				elements.push_back(item);
+			}
+
+			return elements;
+		}
+	};
+}

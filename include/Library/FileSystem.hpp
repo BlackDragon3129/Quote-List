2477877c000da929657510c100ae6981f701adb1
BlackDragon3129 <dayte.akkaunt.2010@gmail.com>
2026-07-08 20:57:26 +0300

    Moved file system functions to the special file

--- include/Library/FileSystem.hpp
+++ include/Library/FileSystem.hpp
@@ -1,1 +1,62 @@
 #pragma once
+
+#include <Windows.h>
+#include <iostream>
+#include <fstream> 
+#include <locale>
+#include <codecvt>
+
+
+namespace QuoteList
+{
+	struct FileSystem
+	{
+		static std::wstring GetAppDataPath()
+		{
+			PWSTR pszPath = NULL;
+			GUID folderId = FOLDERID_LocalAppData;
+
+			HRESULT hr = SHGetKnownFolderPath(folderId, 0, NULL, &pszPath);
+
+			if (SUCCEEDED(hr))
+			{
+				std::wstring path(pszPath);
+				CoTaskMemFree(pszPath);
+				return path + L"\\";
+			}
+
+			return L"";
+		}
+
+
+		static void CreateFolder(const wchar_t* path)
+		{
+			SHCreateDirectoryExW(NULL, path.c_str(), NULL);
+		}
+
+
+		static void WriteFile(const wchar_t* targetPath)
+
+
+		static std::wstring ReadFile(const wchar_t* targetPath)
+		{
+			std::wifstream file(targetPath);
+			file.imbue(std::locale(std::locale::empty(), new std::codecvt_utf8<wchar_t>));
+
+			std::wstring data = L"";
+
+			std::wstring line;
+			if (saveFile.is_open())
+			{
+				while (std::getline(saveFile, line))
+				{
+					data.append(line + L'\n');
+				}
+			}
+			else
+			{
+				return L"";
+			}
+		}
+	};
+}

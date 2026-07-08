48df8158c476b7d0583576fe863963b8f2821d7f
BlackDragon3129 <dayte.akkaunt.2010@gmail.com>
2026-07-08 20:37:59 +0300

    Moved key codes to the special file

--- include/BaseLibrary.hpp
+++ include/BaseLibrary.hpp
@@ -13,16 +13,7 @@
 
 namespace QuoteList
 {
-	struct KeyCode
-	{
-		static const char UP_ARROW = 72;
-		static const char DOWN_ARROW = 80;
-		static const char LEFT_ARROW = 75;
-		static const char RIGHT_ARROW = 77;
-
-		static const char ENTER = 13;
-		static const char ESCAPE = 27;
-	};
+	
 
 
 	static int Randint(const int& min, const int& max)
